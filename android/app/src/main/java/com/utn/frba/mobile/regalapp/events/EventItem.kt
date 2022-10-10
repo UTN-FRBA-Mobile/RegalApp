ea5a0b65857d755c7816691a17e8f90014de1272
Federico Torres <tmiguelangel@frba.utn.edu.ar>
2022-10-10 18:49:31 -0300

    fixed merge error

--- android/app/src/main/java/com/utn/frba/mobile/regalapp/events/EventItem.kt
+++ android/app/src/main/java/com/utn/frba/mobile/regalapp/events/EventItem.kt
@@ -1,6 +1,14 @@
 package com.utn.frba.mobile.regalapp.events
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
