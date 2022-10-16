aa9a52332e2cc543d4c6894b1263024051d832fc
Gonzalo Maidan <gonzalo_maidan@hotmail.com>
2022-10-16 19:31:53 -0300

    change Event model to add item list

--- android/app/src/main/java/com/utn/frba/mobile/regalapp/events/EventModel.kt
+++ android/app/src/main/java/com/utn/frba/mobile/regalapp/events/EventModel.kt
@@ -1,6 +1,9 @@
 package com.utn.frba.mobile.regalapp.events
 
-data class EventModel(val name: String) {
+data class EventModel(
+    val name: String,
+    val items: List<ItemModel> = mutableListOf<ItemModel>()
+) {
 //    var due_date = due_date;
 //    var photo = photo;
 }
