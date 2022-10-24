817872b60f08550abbd2c1064e88596d8edb3024
Federico Torres <tmiguelangel@frba.utn.edu.ar>
2022-10-24 19:27:32 -0300

    query events

--- android/domain/src/main/java/com/utn/frba/mobile/domain/models/EventModel.kt
+++ android/domain/src/main/java/com/utn/frba/mobile/domain/models/EventModel.kt
@@ -2,5 +2,11 @@ package com.utn.frba.mobile.domain.models
 
 data class EventModel(
     val id: String? = null,
-    val name: String = ""
+    val name: String = "",
+    val ownerId: String
 )
+
+enum class EventFields(val value: String) {
+    NAME("NAME"),
+    OWNER_ID("owner_id")
+}
--- android/domain/src/main/java/com/utn/frba/mobile/domain/repositories/events/FirebaseEventsRepositoryImpl.kt
+++ android/domain/src/main/java/com/utn/frba/mobile/domain/repositories/events/FirebaseEventsRepositoryImpl.kt
@@ -5,21 +5,28 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.squareup.anvil.annotations.ContributesBinding
 import com.utn.frba.mobile.domain.DBCollections
 import com.utn.frba.mobile.domain.di.AppScope
+import com.utn.frba.mobile.domain.models.EventFields
 import com.utn.frba.mobile.domain.models.EventModel
 import com.utn.frba.mobile.domain.models.NetworkResponse
+import com.utn.frba.mobile.domain.utils.FirestoreHelper
 import com.utn.frba.mobile.domain.utils.safeCall
 import kotlinx.coroutines.tasks.await
+import java.util.UUID
 import javax.inject.Inject
 
 @ContributesBinding(AppScope::class)
 @AppScope
 class FirebaseEventsRepositoryImpl @Inject constructor(
-    private val db: FirebaseFirestore
-): EventsRepository {
-    override suspend fun fetchEvents(): NetworkResponse<List<EventModel>> = safeCall {
-        val queryResult = getEventsCollection().get().await()
-        val events =  queryResult.documents.mapNotNull {
-            it.toObject(EventModel::class.java)
+    private val db: FirebaseFirestore,
+    private val helper: FirestoreHelper
+) : EventsRepository {
+    override suspend fun fetchUserEvents(userId: String): NetworkResponse<List<EventModel>> = safeCall {
+        val queryResult = getEventsCollection()
+            .whereEqualTo(EventFields.OWNER_ID.value, userId)
+            .get()
+            .await()
+        val events = queryResult.documents.mapNotNull {
+            helper.mapDocumentToEventModel(it)
         }
 
         return NetworkResponse.Success(events)
