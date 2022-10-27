6fb4b85198951491f5dcaff8749b62d88904e460
Gonzalo Maidan <gonzalo_maidan@hotmail.com>
2022-10-27 20:38:27 -0300

    agrego preview de item cards

--- android/app/src/main/java/com/utn/frba/mobile/regalapp/itemList/ItemCard.kt
+++ android/app/src/main/java/com/utn/frba/mobile/regalapp/itemList/ItemCard.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.utn.frba.mobile.domain.models.ItemModel
@@ -16,7 +17,9 @@ import com.utn.frba.mobile.regalapp.R
 @Composable
 fun ItemCard(item: ItemModel, onClick: (ItemModel) -> Unit) {
     Row(
-        Modifier.fillMaxWidth().height(60.dp),
+        Modifier
+            .fillMaxWidth()
+            .height(60.dp),
     ) {
         Button(
             onClick = {
@@ -25,7 +28,9 @@ fun ItemCard(item: ItemModel, onClick: (ItemModel) -> Unit) {
             Modifier.weight(1F),
         ) {
             Row(
-                modifier = Modifier.fillMaxSize().padding(vertical = 5.dp, horizontal = 5.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(vertical = 5.dp, horizontal = 5.dp),
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween,
             ) {
@@ -58,4 +63,24 @@ fun ItemCard(item: ItemModel, onClick: (ItemModel) -> Unit) {
             }
         }
     }
+}
+
+@Preview
+@Composable
+fun ItemCardPendingPreview() {
+    val item = ItemModel(
+        name = "Vasos",
+    )
+    ItemCard(item = item, onClick = {})
+}
+
+@Preview
+@Composable
+fun ItemCardCompletedPreview() {
+    val item = ItemModel(
+        name = "Vasos",
+        status = true,
+        bought_by = "Gonzalo"
+    )
+    ItemCard(item = item, onClick = {})
 }
