3ac105280023264766480b6c98a40cfcd11678ad
Gonzalo Maidan <gonzalo_maidan@hotmail.com>
2022-11-26 19:11:37 -0300

    agrego pedir permisos de ubicacion

--- android/app/src/main/java/com/utn/frba/mobile/regalapp/MainActivity.kt
+++ android/app/src/main/java/com/utn/frba/mobile/regalapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.utn.frba.mobile.regalapp
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker.PERMISSION_GRANTED
 import com.utn.frba.mobile.regalapp.di.DaggerFragmentFactory
 import javax.inject.Inject
 
@@ -15,6 +18,20 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.fragmentFactory = daggerFragmentFactory
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(
+            ActivityCompat.checkSelfPermission(
+                this,
+                ACCESS_COARSE_LOCATION
+            ) != PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    ACCESS_COARSE_LOCATION
+                ),
+                99
+            )
+        }
     }
 
     private fun injectDagger() {
